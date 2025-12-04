a66213a5d7034571f2b216c6d75b7515291ac6a3
Administrator <Administrator@PMT2029>
2025-12-04 13:03:47 +0700

    buoi5

--- nhapmonlaptrinh/Buoi5/Buoi5.cpp
+++ nhapmonlaptrinh/Buoi5/Buoi5.cpp
@@ -16,9 +16,30 @@ void cau1() {
 
 
 }
+void cau2() {
+	float a, b;
+	cout << "Nhap a, b: ";
+	cin >> a >> b;
+	if (a * b > 0)
+		cout << "cungdau";
+	else
+		cout << "nguoc dau";
+}
+void cau3() {
+	int a, b;
+	cout << "Nhap a, b: ";
+	cin >> a >> b;
+	if (a == 0) {
+		if (b == 0)
+			cout << "Phuong trinh vo so nghiem " << endl;
 
+	}
+	else {
+		cout << "Nghiem la: " << (-b * 1.0 / a) << endl;
+	}
+}
 int main()
 {
-	cau1();
+	cau3();
 }
 
